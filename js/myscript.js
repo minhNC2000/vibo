8296957827f3c00b0c86a49c7ee20c16f814d1e3
minhnc2000 <mrminhpro1104@gmail.com>
2022-09-02 17:15:39 +0700

    v1

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+var age = 16;
+// if (age > 18) {
+//   console.log("Duoc uong ruou");
+// } else {
+//   console.log("Khong duoc uong ruou");
+// }
+
+age > 18 ? console.log('hiiiii') :  console.log("Khong duoc uong ruou");
